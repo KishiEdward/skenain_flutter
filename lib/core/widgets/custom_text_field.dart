a72e43171145b66d76225b8a62e6c3835560d93e
KishiEdward <dzidanrh@gmail.com>
2026-04-12 11:59:48 +0700

    membuat custom text untuk widgets

---
+++ lib/core/widgets/custom_text_field.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../constants/app_colors.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final String hint;
+  final bool isPassword;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final TextInputType keyboardType;
+
+  const CustomTextField({
+    super.key,
+    required this.label,
+    required this.hint,
+    required this.controller,
+    this.isPassword = false,
+    this.validator,
+    this.keyboardType = TextInputType.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: AppColors.textPrimary,
+            fontSize: 14,
+          ),
+        ),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: controller,
+          obscureText: isPassword,
+          validator: validator,
+          keyboardType: keyboardType,
+          style: const TextStyle(color: AppColors.textPrimary),
+          decoration: InputDecoration(
+            hintText: hint,
+          ),
+        ),
+      ],
+    );
+  }
+}
