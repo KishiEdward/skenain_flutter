ecb6e34cfbd676d9bd029b540d9d206d17bcbcb7
KishiEdward <dzidanrh@gmail.com>
2026-04-13 21:29:04 +0700

    finalisasi main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; 
+import 'firebase_options.dart';
+
+import 'features/auth/presentation/providers/auth_provider.dart';
+import 'features/dashboard/presentation/providers/product_provider.dart';
+
+import 'core/routes/auth_guard.dart';
+import 'features/auth/presentation/pages/login_page.dart';
+import 'features/auth/presentation/pages/register_page.dart';
+import 'features/auth/presentation/pages/verify_email_page.dart';
+import 'features/dashboard/presentation/pages/dashboard_page.dart';
 
 void main() async {
+  // Wajib untuk inisialisasi Firebase & Flutter
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // Inisialisasi Firebase menghubungkan aplikasi ke project 'verifikasi-email'
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  runApp(
+    // Mendaftarkan semua Provider di akar aplikasi
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+      ],
+      child: const SkienaApp(),
+    ),
   );
-
-  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SkienaApp extends StatelessWidget {
+  const SkienaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Skenain',
+      title: 'Skiena',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Sistem Firebase Skenain Siap!',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF5D4037)),
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
       ),
+
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const AuthGuard(),
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/verify-email': (context) => const VerifyEmailPage(),
+        '/dashboard': (context) => const DashboardPage(),
+      },
     );
   }
 }
